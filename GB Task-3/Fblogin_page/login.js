33dd4df1d3812b59cf799a24e61e3a908074ff69
Manav Dubey <116270475+manav-dubey@users.noreply.github.com>
2024-06-04 20:03:46 +0530

    Add files via upload
    3rd task fb-signupPage

---
+++ GB Task-3/Fblogin_page/login.js
@@ -0,0 +1,6 @@
+let btn_create = document.getElementById('btn2');
+btn_create.addEventListener('click' , open_signup_page);
+
+function open_signup_page () {
+    window.location.href = 'signup.html';
+}
