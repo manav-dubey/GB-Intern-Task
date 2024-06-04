1e8bcad812c56b7f8b0b31d9cb5ec81cd81c97ae
Manav Dubey <116270475+manav-dubey@users.noreply.github.com>
2024-06-04 17:12:49 +0530

    Add files via upload
    FB-login/signup page codes

---
+++ Fblogin_page/signup.js
@@ -0,0 +1,18 @@
+document.getElementById('btn2').addEventListener('click',ageFunc);
+
+function ageFunc(){
+    
+    let year = document.getElementById('year').value ;
+    if (year === '') {
+        alert("Please select a valid year.");
+        return;
+    }
+    
+    let age = 2024 - parseInt(year) ;
+    if(age <18){
+        alert("You are underage");
+        return ;
+    }
+
+}
+
